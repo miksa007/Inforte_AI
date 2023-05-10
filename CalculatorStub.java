34322167f4b14b6fff2f513907aaae0fcaf40774
Mika Saari <mika.saari@tuni.fi>
2023-05-10 18:56:30 +0300

    exercise materials

---
+++ CalculatorStub.java
@@ -0,0 +1,34 @@
+/**Create calculator
+ */
+
+public class CalculatorStub {
+
+    // Method for addition
+    public static int add(int num1, int num2) {
+        // Implement this method
+        return 0;
+    }
+
+    // Method for subtraction
+    public static int subtract(int num1, int num2) {
+        // Implement this method
+        return 0;
+    }
+
+    // Method for multiplication
+    public static int multiply(int num1, int num2) {
+        // Implement this method
+        return 0;
+    }
+
+    // Method for division
+    public static double divide(int num1, int num2) throws ArithmeticException{
+        // Implement this method
+        return 0;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Test your methods here
+    }
+}
